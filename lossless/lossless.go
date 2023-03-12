497f6be269879bdc34d05fc712bf9cdef91c9305
Frederico Pinto <fredmnpinto@gmail.com>
2023-03-12 02:54:38 +0000

    fix: Fixing golint complaints

--- lossless/lossless.go
+++ lossless/lossless.go
@@ -2,13 +2,13 @@ package lossless
 
 import "image"
 
-// Writes the image encoded in Lossless JPEG to the file
+// Encode writes the image encoded in Lossless JPEG to the file
 func Encode(image image.YCbCr, filename string) (err error) {
 
 	return nil
 }
 
-// Reads the image encoded in Lossless JPEG from the file
+// Decode reads the image encoded in Lossless JPEG from the file
 func Decode(image image.YCbCr, filename string) (decodedImage image.YCbCr, err error) {
 
 	return decodedImage, nil
